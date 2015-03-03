447dad4a587f38c606bfeea8c5fd6ed66e250a94
shramko82 <shramko82@gmail.com>
2015-03-03 03:54:54 +0200

    Less iterations
    Less iterations

--- src/FindingLargestPalindrome.java
+++ src/FindingLargestPalindrome.java
@@ -1,32 +1,31 @@
 
 public class FindingLargestPalindrome {
-
+	
 	public static void main(String[] args) {
-		int biggest = 0, maxI = 0, maxJ = 0;
+		int largestPalindrome = 0, maxI = 0, maxJ = 0, n = 0;
 		for (int i = 999; i > 99; i--) {
 			for (int j = 999; j>=i; j--) {
 				int result = i*j; 
+				n++;
+				if (result < largestPalindrome) {
+					if (j == 999) i = 0;
+					break;
+				}
 				String resultStr = String.valueOf(result); 
-				// we use StringBuilder shell because this class has reverse() method
-				// otherwise we should revert String as char array using str.toCharArray()
-				// then use cycle to revert this String
 				StringBuilder resultSB = new StringBuilder(resultStr);
 				String reverseStr = resultSB.reverse().toString();
 				if (resultStr.equals(reverseStr)) {
-					if (result > biggest) {
-						biggest = result;
+					if (result > largestPalindrome) {
+						largestPalindrome = result;
 						maxI = i;
 						maxJ = j;
+						break;
 					}
-					
 				}
-				
 			}
-			
 		}
-		
-		System.out.println("The largest Palindrome number is "+biggest+" = "+maxI+" * "+maxJ);
-
+		System.out.println("The largest Palindrome number is "+largestPalindrome+" = "+maxI+" * "+maxJ);
+		System.out.println("Number of iterations - "+n);
 	}
 
 }
